def669e2b4bb601bb5cb5ff2b87bcfafa24f906f
chengqiang0918 <1349827872@qq.com>
2019-08-22 16:15:09 +0800

    cheng

--- src/main/java/com/j1904/pojo/Dept.java
+++
@@ -1,9 +0,0 @@
-package com.j1904.pojo;
-
-import java.io.Serializable;
-
-public class Dept implements Serializable {
-    private int id;//
-    private String name;//
-    private String sex;//
-}
